43fe07a928f4122ac99123957b324185c90b0a82
Cristian <cristian.niculescu@gmail.com>
2021-09-20 12:38:48 -0700

    working Highcharts column drilldown and building a top5trees object.

--- client/src/components/Chart/Chart.jsx
+++ client/src/components/Chart/Chart.jsx
@@ -1,26 +1,95 @@
 import React, { Component } from 'react'
 import HighchartsReact from 'highcharts-react-official'
 import Highcharts from 'highcharts'
+// import High harts, { chart } from 'highcharts'
 // import accessibility from "highcharts/modules/accessibility";
-import drilldown from "highcharts/modules/drilldown";
+import drilldown from "highcharts/modules/drilldown.js";
+
+drilldown(Highcharts); 
+
+const dataYearsTree1 = [["1990", 50], ["1992", 2], ["1993", 1], ["1997", 2],
+                        ["1999", 1], [ "2002", 6], ["2004", 2], ["2006", 11],
+                        ["2007", 4], [ "2008", 7], ["2010", 1], ["2011", 5],
+                        ["2012", 4], ["2014", 6], ["2017", 1], ["2019", 1]]
+        
+const dataYearsTree2 = [["1990", 50], ["1992", 2], ["1993", 1], ["1997", 2],
+                        ["1999", 1], [ "2002", 6], ["2004", 2], ["2006", 11],
+                        ["2007", 4], ["2008", 7], ["2010", 1], ["2011", 5]
+                        ["2012", 4], ["2014", 6], ["2017", 1], ["2019", 1]]
+
+const dataYearsTree3 = [["1990", 20], ["1992", 10], ["1993", 1], ["1994", 16],
+                        ["1996", 1], ["1997", 4], ["2000", 4], ["2003", 1],
+                        ["2004", 1], ["2005", 8]]
+
+const dataYearsTree4 = [["1990", 8], ["1991", 25], ["1992", 12], ["2009", 1]]
+
+const dataYearsTree5 = [["1990", 1], ["1992", 1], ["1993", 1], ["1999", 1],
+                        ["2002", 1], [ "2007", 1], ["2009", 15]]
+
+const top5Trees = {
+  tree1:
+    { name: "Kwanzan Flowering Cherry", count: 747, years: dataYearsTree1 },
+  tree2:
+    { name: "Maple", count: 800, years: dataYearsTree2 }
+}
+
+const neighbourhood = "Mount Pleasant"
+
+// console.log('top5Trees:', top5Trees)
+// console.log("Tree2.name: --> ", top5Trees.tree1.years)
 
 
-// const options = {
-//   title: {
-//     text: 'My chart'
-//   },
-//   series: [{
-//     data: [1, 2, 3, 5, 6, 3, 1]
-//   }]
-// }
 
-const options = {
+const chartOptions = {
   chart: {
-    type: 'column',
-    events: {}
+    type: "column",
+    events: {
+      drilldown: function (e) {
+        if (!e.seriesOptions) {
+          var chart = this;
+          if (e.point.name === top5Trees.tree1.name) {
+            chart.addSingleSeriesAsDrilldown(e.point, {
+              name: top5Trees.tree1.name,
+              color: "green",
+              // data: dataYearsTree1
+              data: top5Trees.tree1.years
+            })
+          }
+          if (e.point.name === "Red Maple") {
+            chart.addSingleSeriesAsDrilldown(e.point, {
+              name: "Red Maple",
+              color: "green",
+              data: dataYearsTree2
+            })
+          }
+          if (e.point.name === "Chanticleer Pear") {
+            chart.addSingleSeriesAsDrilldown(e.point, {
+              name: "Chanticleer Pear",
+              color: "green",
+              data: dataYearsTree3
+            })
+          }
+          if (e.point.name === "Crimean Linden") {
+            chart.addSingleSeriesAsDrilldown(e.point, {
+              name: "Chanticleer Pear",
+              color: "green",
+              data: dataYearsTree4
+            })
+          }
+          if (e.point.name === "Norway Maple") {
+            chart.addSingleSeriesAsDrilldown(e.point, {
+              name: "Chanticleer Pear",
+              color: "green",
+              data: dataYearsTree5
+            })
+          }
+          chart.applyDrilldown();
+        }
+      }
+    }
   },
   title: {
-    text: 'Top 5 types of trees planted in Mount Pleasant'
+    text: "Top 5 types of trees planted in " + neighbourhood
   },
   // accessibility: {
   //   announceNewData: {
@@ -49,248 +118,38 @@ const options = {
       colorByPoint: true,
       data: [
         {
-          name: "Kwanzan Flowering Cherry",
-          y: 747,
-          drilldown: "Kwanzan Flowering Cherry"
+          name: top5Trees.tree1.name,
+          y: top5Trees.tree1.count,
+          drilldown: true
         },
         {
           name: "Red Maple",
           y: 264,
-          drilldown: "Red Maple"
+          drilldown: true
         },
         {
           name: "Chanticleer Pear",
           y: 239,
-          drilldown: "Chanticleer Pear"
+          drilldown: true
         },
         {
           name: "Crimean Linden",
           y: 219,
-          drilldown: "Crimean Linden"
+          drilldown: true
         },
         {
           name: "Norway Maple",
           y: 217,
-          drilldown: "Norway Maple"
+          drilldown: true
         },
       ]
     }
   ],
-  // events: {
-    drilldown: {
-      series: [
-        {
-          name: "Kwanzan Flowering Cherry",
-          id: "Kwanzan Flowering Cherry",
-          data: [
-            [
-              "1990",
-              50
-            ],
-            [
-              "1992",
-              2
-            ],
-            [
-              "1993",
-              1
-            ],
-            [
-              "1997",
-              2
-            ],
-            [
-              "1999",
-              1
-            ],
-            [
-              "2002",
-              6
-            ],
-            [
-              "2004",
-              2
-            ],
-            [
-              "2006",
-              11
-            ],
-            [
-              "2007",
-              4
-            ],
-            [
-              "2008",
-              7
-            ],
-            [
-              "2010",
-              1
-            ],
-            [
-              "2011",
-              5
-            ],
-            [
-              "2012",
-              4
-            ],
-            [
-              "2014",
-              6
-            ],
-            [
-              "2017",
-              1
-            ],
-            [
-              "2019",
-              1
-            ]
-          ]
-        },
-        {
-          name: "Firefox",
-          id: "Firefox",
-          data: [
-            [
-              "v58.0",
-              1.02
-            ],
-            [
-              "v57.0",
-              7.36
-            ],
-            [
-              "v56.0",
-              0.35
-            ],
-            [
-              "v55.0",
-              0.11
-            ],
-            [
-              "v54.0",
-              0.1
-            ],
-            [
-              "v52.0",
-              0.95
-            ],
-            [
-              "v51.0",
-              0.15
-            ],
-            [
-              "v50.0",
-              0.1
-            ],
-            [
-              "v48.0",
-              0.31
-            ],
-            [
-              "v47.0",
-              0.12
-            ]
-          ]
-        },
-        {
-          name: "Internet Explorer",
-          id: "Internet Explorer",
-          data: [
-            [
-              "v11.0",
-              6.2
-            ],
-            [
-              "v10.0",
-              0.29
-            ],
-            [
-              "v9.0",
-              0.27
-            ],
-            [
-              "v8.0",
-              0.47
-            ]
-          ]
-        },
-        {
-          name: "Safari",
-          id: "Safari",
-          data: [
-            [
-              "v11.0",
-              3.39
-            ],
-            [
-              "v10.1",
-              0.96
-            ],
-            [
-              "v10.0",
-              0.36
-            ],
-            [
-              "v9.1",
-              0.54
-            ],
-            [
-              "v9.0",
-              0.13
-            ],
-            [
-              "v5.1",
-              0.2
-            ]
-          ]
-        },
-        {
-          name: "Edge",
-          id: "Edge",
-          data: [
-            [
-              "v16",
-              2.6
-            ],
-            [
-              "v15",
-              0.92
-            ],
-            [
-              "v14",
-              0.4
-            ],
-            [
-              "v13",
-              0.1
-            ]
-          ]
-        },
-        {
-          name: "Opera",
-          id: "Opera",
-          data: [
-            [
-              "v50.0",
-              0.96
-            ],
-            [
-              "v49.0",
-              0.82
-            ],
-            [
-              "v12.1",
-              0.14
-            ]
-          ]
-        }
-      ]
-    // }
+  drilldown: {
+    series: []
   }
 };
+
 
 
 class Chart extends Component {
@@ -301,7 +160,7 @@ class Chart extends Component {
         <HighchartsReact
           highcharts={Highcharts}
           // constructorType={'column'}
-          options={options}
+          options={chartOptions}
         />
       </div>
     )
