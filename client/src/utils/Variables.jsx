2f78cc1f482fcdbd4439b0629043bc44145c1d16
Cristian <cristian.niculescu@gmail.com>
2021-10-23 14:16:41 -0700

    variables file, also includes some placeholders for future use

---
+++ client/src/utils/Variables.jsx
@@ -0,0 +1,102 @@
+export const neighbourhoods = [
+  "Downtown",
+  "Mount Pleasant",
+  "West End",
+  "Strathcona",
+  "Shaughnessy",
+  "Stanley Park",
+  "Grandview-Woodland",
+  "Kensington-Cedar Cottage",
+  "Kitsilano",
+  "Fairview",
+  "Marpole",
+  "RileyPark",
+  "Oakridge",
+  "Sunset",
+  "Hastings-Sunrise",
+  "Killarney",
+  "South Cambie",
+  "Arbutus Ridge",
+  "West Point Grey",
+]
+
+export const neighbourhoodsDropdown = [
+  { key: 0,  value: 'DOWNTOWN',                  text: 'Downtown' },
+  { key: 1,  value: 'MOUNT+PLEASANT',            text: 'Mount Pleasant' },
+  { key: 2,  value: 'WEST+END',                  text: 'West End' },
+  { key: 3,  value: 'STRATHCONA',                text: 'Strathcona' },
+  { key: 4,  value: 'SHAUGHNESSY',               text: 'Shaughnessy' },
+  { key: 5,  value: 'STANLEY+PARK',              text: 'Stanley Park' },
+  { key: 6,  value: 'GRANDVIEW-WOODLAND',        text: 'Grandview-Woodland' },
+  { key: 7,  value: 'KENSINGTON-CEDAR+COTTAGE',  text: 'Kensington-Cedar Cottage' },
+  { key: 8,  value: 'KITSILANO',                 text: 'Kitsilano' },
+  { key: 9,  value: 'FAIRVIEW',                  text: 'Fairview' },
+  { key: 10, value: 'MARPOLE',                   text: 'Marpole' },
+  { key: 11, value: 'RILEY+PARK',                text: 'RileyPark' },
+  { key: 12, value: 'OAKRIDGE',                  text: 'Oakridge' },
+  { key: 13, value: 'SUNSET',                    text: 'Sunset' },
+  { key: 14, value: 'HASTINGS-SUNRISE',          text: 'Hastings-Sunrise' },
+  { key: 15, value: 'KILLARNEY',                 text: 'Killarney' },
+  { key: 16, value: 'SOUTH+CAMBIE',              text: 'South Cambie' },
+  { key: 17, value: 'ARBUTUS-RIDGE',             text: 'Arbutus Ridge' },
+  { key: 18, value: 'WEST+POINT+GREY',           text: 'West Point Grey' },
+]
+
+// TODO: Double check value's match COV API
+
+
+//------------------------ Map ----------------------------------//
+export const neighbourhoodCenters = []
+
+export const legoManPosition = [49.246292, -123.116226]
+
+export const vancouver = [49.246292, -123.116226]
+
+// TODO: Add key to CircleMarker
+// <CircleMarker
+//   // key={'cm-' + this.props.series.map(name => name.name)[0]}
+
+
+//------------------- Colors (Chart, Map Markers, Map Controls) -----------------------------//
+// Source, Color Advisor: https://colorbrewer2.org/#type=qualitative&scheme=Paired&n=7 
+
+export const colors = ['#6a3d9a', '#1f78b4', '#b15928', '#33a02c', '#ff7f00']  // <-- darker, the one used
+
+export const colors2 = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99']
+export const colors3 = ['#b2df8a', '#33a02c', '#fb9a99', '#e31a1c', '#fdbf6f']
+
+export const mapBtnBkgColor = [ "#FsdFFF" ]
+
+// -- Possible Graph Colours --
+// Light Blue:  rgb(124, 181, 236) /  websafe #ccff66  / hex #b5ec7c
+// Brown:       rgb(67, 67, 72)    /  websafe #333333  / hex #434348
+// Purple:      rgb(128, 133, 233) /  websafe #9999ff  / hex #8085e9
+      
+// ----------------- Place Holders ----------------------- // 
+
+export const trees = [
+	"Kwanzan Flowering Cherry", "Red Maple", "Chanticleer Pear",
+	"Crimean Linden", "Norway Maple",
+]
+
+// export const neighbourhoodsDropdown = [
+//   { key: 'dt', value: 'DOWNTOWN',                 text: 'Downtown' },
+//   { key: 'mp', value: 'Mount Pleasant',           text: 'Mount Pleasant' },
+//   { key: 'we', value: 'West End',                 text: 'West End' },
+//   { key: 'st', value: 'Strathcona',               text: 'Strathcona' },
+//   { key: 'sh', value: 'Shaughnessy',              text: 'Shaughnessy' },
+//   { key: 'sp', value: 'Stanley Park',             text: 'Stanley Park' },
+//   { key: 'gw', value: 'Grandview-Woodland',       text: 'Grandview-Woodland' },
+//   { key: 'kc', value: 'Kensington-Cedar Cottage', text: 'Kensington-Cedar Cottage' },
+//   { key: 'ki', value: 'Kitsilano',                text: 'Kitsilano' },
+//   { key: 'fa', value: 'Fairview',                 text: 'Fairview' },
+//   { key: 'ma', value: 'Marpole',                  text: 'Marpole' },
+//   { key: 'rp', value: 'RileyPark',                text: 'RileyPark' },
+//   { key: 'oa', value: 'Oakridge',                 text: 'Oakridge' },
+//   { key: 'su', value: 'Sunset',                   text: 'Sunset' },
+//   { key: 'hs', value: 'Hastings-Sunrise',         text: 'Hastings-Sunrise' },
+//   { key: 'kl', value: 'Killarney',                text: 'Killarney' },
+//   { key: 'sc', value: 'South Cambie',             text: 'South Cambie' },
+//   { key: 'ar', value: 'Arbutus Ridge',            text: 'Arbutus Ridge' },
+//   { key: 'wp', value: 'West Point Grey',          text: 'West Point Grey' },
+// ]
