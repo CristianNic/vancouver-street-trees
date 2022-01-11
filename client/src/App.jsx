f2e6241b62383e410d8b093d9065170e6eac4311
Cristian <cristian.niculescu@gmail.com>
2022-01-11 00:34:10 -0800

    added space

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard"
+
 class App extends Component {
 
   render() {
