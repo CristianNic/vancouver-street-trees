264c2abbca4ae5420c0a9b475f7ec8cc40d2b1ae
Cristian <cristian.niculescu@gmail.com>
2022-01-10 23:53:55 -0800

    simplified down to one line

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-// import iconEmail from '../../assets/icons-feather-1.5px/mail.svg';
-// import iconGithub from '../../assets/icons-feather-1.5px/github.svg';
-// import iconLinkedIn from '../../assets/icons-feather-1.5px/linkedin.svg';
-
-const sourceUrl = "https://opendata.vancouver.ca/explore/dataset/street-trees/information/?disjunctive.species_name&disjunctive.common_name&disjunctive.height_range_id"
+import { Divider } from "semantic-ui-react";
 
 class Footer extends Component {
 
@@ -11,34 +7,11 @@ class Footer extends Component {
 
     return (
       <footer className="footer">
-        {/* <div className="footer__about">
-          <h1 className="about__name">Cristian Niculescu</h1>
-          <div className="about__link-container">
-            <a className="about__link" href="mailto:Cristian.Niculescu@gmail.com">
-              <img className="about__icon" src={iconEmail} alt="email icon" />
-              <h4 className="about__text">Cristian.Niculescu@gmail.com</h4>
-            </a>
-            <a className="about__link" href="https://github.com/CristianNic">
-              <img className="about__icon" src={iconGithub} alt="github icon" />
-              <h4 className="about__text">Github.com/CristianNic</h4>
-            </a>
-            <a className="about__link" href="https://github.com/CristianNic">
-              <img className="about__icon" src={iconLinkedIn} alt="linkedin icon" />
-              <h4 className="about__text">Linkedin.com/in/cristian-niculescu/</h4>
-            </a>
-          </div>
-        </div> */}
-        <a href={sourceUrl}>
-          <h3 className="footer__data-source">
-            <span className="black bold">Find out more: </span>
-            <span className="black underline"> City of Vancouver Open Data Portal, Street Trees</span>
-          </h3>
-        </a>
+        <Divider></Divider>
+          <h2 className="footer__made-with">Made with <span style={{ color: '#e25555' }}>&nbsp;&#9829;&nbsp;</span> in Vancouver</h2>        
         </footer>
     );
   }
 }
 
 export default Footer;
-
-// TODO: Make with love & My info w/ Link to GitHub & LinkedIn? 
