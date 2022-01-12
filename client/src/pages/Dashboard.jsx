88e3da307d57fc84da27b0d6522081c9f5d6269a
Cristian <cristian.niculescu@gmail.com>
2022-01-11 18:20:06 -0800

    refactored, getData makes api call for either all of vancouver or one neighbourhood, while extractData parses either response

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -34,7 +34,7 @@ class Dashboard extends Component {
   
   componentDidMount() {
     const selectedNeighbourhood = this.state.neighbourhood
-    this.getData(selectedNeighbourhood)
+    this.extractData(selectedNeighbourhood)
   }
   
   yearlyCount(years, count) {
@@ -45,10 +45,10 @@ class Dashboard extends Component {
     }
     return yearlyCountArr
   }
-  
-  async getData(selectedNeighbourhood) {
 
+  async getData(selectedNeighbourhood) {
     if (selectedNeighbourhood === 'VANCOUVER') {
+      const area = 'Vancouver'
       const API_URL_ALL_VANCOUVER = street_trees_dataset_url + rows_0 + facet_common_name
       const getAllVancouverTrees = await axios.get(`${API_URL_ALL_VANCOUVER}`)
       
@@ -61,85 +61,23 @@ class Dashboard extends Component {
 
       //--- Get info on each of the top 5 trees ---//
       const api_formatted_tree_common_name = await top5TreeNames.map(treeName => treeName.replaceAll(' ', '+'))
-      
       const API_URL = street_trees_dataset_url + rows_10000 + facet_common_name + facet_date_planted + refine_common_name
-
       const getTreeInfoPromises = []
       api_formatted_tree_common_name.forEach(treeName => {
         getTreeInfoPromises.push(axios.get(`${API_URL}${treeName}`))
       })
-
       const getTreeInfo = await Promise.all(getTreeInfoPromises)
       const treeInfo = getTreeInfo.map(treeInfo => treeInfo.data.facet_groups[1].facets)
 
-      //--- For each tree, find how many trees were planted and in which years ---//
-      const years = []
-      treeInfo.forEach(tree => {
-        const yearArray = tree.map(year => year.name)
-        years.push(yearArray)
-      })
-
-      const count = []
-      treeInfo.forEach(tree => {
-        const countArray = tree.map(tree => tree.count)
-        count.push(countArray)
-      })
-
-      //--- Sum total of the top 5 tree types to display on graph ---//
-      const reducer = (previousValue, currentValue) => previousValue + currentValue
-      const totalTop5 = top5TreesByCount.reduce(reducer)
-      const top5TotalPlanted = new Intl.NumberFormat().format(totalTop5)
-
-      //--- Get Lat/Lng coordinates and for each tree type and create a separate geom object here so ---//
-      //--- the map component can quickly map over each. Remove trees that don't have a geom record  ---//
-      const treeRecords = getTreeInfo.map(treeInfo => treeInfo.data.records)
-
-      const geom = []
-      treeRecords.forEach(record => {
-        const treeGeom = record.map(geoms => geoms.fields.geom)
-        const removeUndefined = treeGeom.filter(geoms => geoms !== undefined)
-        const coordinates = removeUndefined.map(co => co.coordinates)
-        geom.push(coordinates)
-      })
-      const geom0 = geom[0]
-      const geom1 = geom[1]
-      const geom2 = geom[2]
-      const geom3 = geom[3]
-      const geom4 = geom[4]
-    
-      //--- Make an organized tree data object for the chart and possibly more future components ---//
-      const top5TreeData = []
-      for (let i = 0; i < treeInfo.length; i++) {
-        const obj = {
-          name: top5TreeNamesLowerCaps[i],
-          totalCount: top5TreesByCount[i],
-          yearlyCount: this.yearlyCount(years[i], count[i]),
-          geom: geom[i]
-        }
-        top5TreeData.push(obj)
-      }
-      // console.log('Vancouver-top5TreeData:', top5TreeData[0])
-    
-      this.setState({
-        neighbourhood: 'Vancouver',
-        top5TreeData: top5TreeData,
-        top5TreeNamesLowerCaps: top5TreeNamesLowerCaps,
-        top5TotalPlanted: top5TotalPlanted,
-        geom: geom,
-        geom0: geom0,
-        geom1: geom1,
-        geom2: geom2,
-        geom3: geom3,
-        geom4: geom4,
-        activeTrees: [true, true, true, true, true]
-      })
+      return { area, top5TreeNamesLowerCaps, top5TreesByCount, getTreeInfo, treeInfo }
 
     } else {
       const API_URL_NEIGHBOURHOOD = street_trees_dataset_url + rows_0 + facet_common_name + refine_neighbourhood + selectedNeighbourhood
       const getAllNeighbourhoodTrees = await axios.get(`${API_URL_NEIGHBOURHOOD}`)
 
       const confirmNeighbourhood = getAllNeighbourhoodTrees.data.facet_groups[1].facets[0].name
-      const confirmNeighbourhoodLowerCaps = capitalizeFirstLetter(confirmNeighbourhood)
+      // const confirmNeighbourhoodLowerCaps = capitalizeFirstLetter(confirmNeighbourhood)
+      const area = capitalizeFirstLetter(confirmNeighbourhood)
 
       const top5TreeCommonNames = getAllNeighbourhoodTrees.data.facet_groups[0].facets.slice(0, 5)
       const top5TreeNames = top5TreeCommonNames.map(treeName => treeName.name)
@@ -162,68 +100,76 @@ class Dashboard extends Component {
       const getTreeInfo = await Promise.all(getTreeInfoPromises)
       const treeInfo = getTreeInfo.map(treeInfo => treeInfo.data.facet_groups[2].facets)
 
-      //--- for each tree, find how many trees were planted and in which years ---//
-      const years = []
-      treeInfo.forEach(tree => {
-        const yearArray = tree.map(year => parseInt(year.name))
-        years.push(yearArray)
-      })
+      return { area, top5TreeNamesLowerCaps, top5TreesByCount, getTreeInfo, treeInfo }
+    } 
+  }
 
-      const count = []
-      treeInfo.forEach(tree => {
-        const countArray = tree.map(tree => tree.count)
-        count.push(countArray)
-      })
+  async extractData(selectedNeighbourhood) {
 
-      //--- Sum total of the top 5 tree types to display on graph ---//
-      const reducer = (previousValue, currentValue) => previousValue + currentValue
-      const totalTop5 = top5TreesByCount.reduce(reducer)
-      const top5TotalPlanted = new Intl.NumberFormat().format(totalTop5)
-
-      //--- Get Lat/Lng coordinates and for each tree type and create a separate geom object here so ---//
-      //--- the map component can quickly map over each. Remove trees that don't have a geom record  ---//
-      const treeRecords = getTreeInfo.map(treeInfo => treeInfo.data.records)
-
-      const geom = []
-      treeRecords.forEach(record => {
-        const treeGeom = record.map(geoms => geoms.fields.geom)
-        const removeUndefined = treeGeom.filter(geoms => geoms !== undefined)
-        const coordinates = removeUndefined.map(co => co.coordinates)
-        geom.push(coordinates)
-      })
-      const geom0 = geom[0]
-      const geom1 = geom[1]
-      const geom2 = geom[2]
-      const geom3 = geom[3]
-      const geom4 = geom[4]
+    const { area, top5TreeNamesLowerCaps, top5TreesByCount, getTreeInfo, treeInfo } = await this.getData(selectedNeighbourhood)
+    console.log('treeInfo:', treeInfo)
     
-      //--- Make an organized tree data object for the chart and possibly more future components ---//
-      const top5TreeData = []
-      for (let i = 0; i < treeInfo.length; i++) {
-        const obj = {
-          name: top5TreeNamesLowerCaps[i],
-          totalCount: top5TreesByCount[i],
-          yearlyCount: this.yearlyCount(years[i], count[i]),
-          geom: geom[i]
-        }
-        top5TreeData.push(obj)
+    //--- For each tree, find how many trees were planted and in which years ---//
+    const years = []
+    treeInfo.forEach(tree => {
+      const yearArray = tree.map(year => year.name)
+      years.push(yearArray)
+    })
+
+    const count = []
+    treeInfo.forEach(tree => {
+      const countArray = tree.map(tree => tree.count)
+      count.push(countArray)
+    })
+
+    //--- Sum total of the top 5 tree types to display on graph ---//
+    const reducer = (previousValue, currentValue) => previousValue + currentValue
+    const totalTop5 = top5TreesByCount.reduce(reducer)
+    const top5TotalPlanted = new Intl.NumberFormat().format(totalTop5)
+
+    //--- Get Lat/Lng coordinates and for each tree type and create a separate geom object here so ---//
+    //--- the map component can quickly map over each. Remove trees that don't have a geom record  ---//
+    const treeRecords = getTreeInfo.map(treeInfo => treeInfo.data.records)
+
+    const geom = []
+    treeRecords.forEach(record => {
+      const treeGeom = record.map(geoms => geoms.fields.geom)
+      const removeUndefined = treeGeom.filter(geoms => geoms !== undefined)
+      const coordinates = removeUndefined.map(co => co.coordinates)
+      geom.push(coordinates)
+    })
+    const geom0 = geom[0]
+    const geom1 = geom[1]
+    const geom2 = geom[2]
+    const geom3 = geom[3]
+    const geom4 = geom[4]
+  
+    //--- Make an organized tree data object for the chart and possibly more future components ---//
+    const top5TreeData = []
+    for (let i = 0; i < treeInfo.length; i++) {
+      const obj = {
+        name: top5TreeNamesLowerCaps[i],
+        totalCount: top5TreesByCount[i],
+        yearlyCount: this.yearlyCount(years[i], count[i]),
+        geom: geom[i]
       }
-      // console.log('Dash-top5TreeData:', top5TreeData[0])
-    
-      this.setState({
-        neighbourhood: confirmNeighbourhoodLowerCaps,
-        top5TreeData: top5TreeData,
-        top5TreeNamesLowerCaps: top5TreeNamesLowerCaps,
-        top5TotalPlanted: top5TotalPlanted,
-        geom: geom,
-        geom0: geom0,
-        geom1: geom1,
-        geom2: geom2,
-        geom3: geom3,
-        geom4: geom4,
-        activeTrees: [true, true, true, true, true]
-      })
+      top5TreeData.push(obj)
     }
+    // console.log('Vancouver-top5TreeData:', top5TreeData[0])
+
+    this.setState({
+      neighbourhood: area,
+      top5TreeData: top5TreeData,
+      top5TreeNamesLowerCaps: top5TreeNamesLowerCaps,
+      top5TotalPlanted: top5TotalPlanted,
+      geom: geom,
+      geom0: geom0,
+      geom1: geom1,
+      geom2: geom2,
+      geom3: geom3,
+      geom4: geom4,
+      activeTrees: [true, true, true, true, true]
+    })
   }
 
   handleDropdown = (e, data) => {
