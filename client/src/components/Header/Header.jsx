805859f282e9d99b91a2a2941f6b21387fe4f166
Cristian <cristian.niculescu@gmail.com>
2022-01-14 20:33:22 -0800

    changed title

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,8 +6,14 @@ class Header extends Component {
 
     return (
       <header className="header">
+        {/* <div className="header__wrapper">
+          <h1 className="header__title">Top Five Trees Planted by the City of Vancouver</h1>
+        </div> */}
         <div className="header__wrapper">
-          <h1 className="header__title">Top 5 Trees Planted by the City of Vancouver</h1>
+          {/* <h1 className="header__title">Top Five Trees Planted by the City of Vancouver</h1> */}
+          {/* <h1 className="header__title">What are your neighbourhood trees? </h1> */}
+          <h1 className="header__title">What trees are in your neighbourhood?</h1>
+          {/* <h1 className="header__title">by the City of Vancouver</h1> */}
         </div>
       </header>
     )
@@ -15,3 +21,8 @@ class Header extends Component {
 }
 
 export default Header;
+
+// {/* <h1 className="header__title">City of Vancouver Top 5 Trees Planted</h1> */}
+// {/* <h1 className="header__title">Top 5 Trees Planted in each Neighbourhood by the City of Vancouver</h1> */}
+// {/* <h1 className="header__title">Top 5 Trees Planted</h1> */}
+// {/* <h1 className="header__title">by the City of Vancouver</h1> */}
